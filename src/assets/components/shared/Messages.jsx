ff3e117bf25426efca3f625caabe2c21ed81fe48
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2024-09-08 02:06:58 +0500

    Fix deployment error

--- src/assets/components/shared/Messages.jsx
+++ src/assets/components/shared/Messages.jsx
@@ -1,31 +1,31 @@
-import { useEffect } from "react"
-import Message from "./Message"
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore"
-import { db } from "../../../Firebase"
-import { useDispatch, useSelector } from "react-redux"
-import { setEmails } from "../../../Redux/appSlice"
-
+import { useEffect } from "react";
+import Message from "./Message";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useDispatch, useSelector } from "react-redux";
+import { setEmails } from "../../../Redux/appSlice";
+import { db } from "../../../firebase";
 
 export const Messages = () => {
-  const dispatch = useDispatch()
-  const emails = useSelector((state) => state.appSlice.emails)
+  const dispatch = useDispatch();
+  const emails = useSelector((state) => state.appSlice.emails);
   useEffect(() => {
-    const q = query(collection(db, "emails"), orderBy("createdAt", "desc"))
+    const q = query(collection(db, "emails"), orderBy("createdAt", "desc"));
     const unsubscribe = onSnapshot(q, (snapshot) => {
-      const allEmails = snapshot.docs.map((doc) => ({...doc.data(), doc:doc.id}))
+      const allEmails = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        doc: doc.id,
+      }));
       // console.log(allEmails)
-      dispatch(setEmails(allEmails))
-    })
-    return () => unsubscribe()
-  },[])
+      dispatch(setEmails(allEmails));
+    });
+    return () => unsubscribe();
+  }, []);
   return (
     <div>
-      {
-        emails && emails?.map((email, index) => (
-          <Message key={index} email={email} />))
-      }
+      {emails &&
+        emails?.map((email, index) => <Message key={index} email={email} />)}
       <Message />
     </div>
-  )
-}
-export default Messages
+  );
+};
+export default Messages;
